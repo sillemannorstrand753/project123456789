d9c46993ebaff814cae5c6ea7da7920e6217b0d9
sillemannorstrand753 <sillemannorstrand753@gmail.com>
2025-03-08 18:40:11 +0000

    Add 3.php

---
+++ src/3.php
@@ -0,0 +1,5 @@
+<?php
+// This is a comment, and it will not be executed by the PHP interpreter
+$message = "Hello, World!"; // This is an example of a variable assignment
+echo $message; // This will output the value of the variable $message, which is "Hello, World!"
+?>
