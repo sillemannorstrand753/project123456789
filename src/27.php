4a74bd12d915241bd918ece7cb22757023d379c6
sillemannorstrand753 <sillemannorstrand753@gmail.com>
2025-04-06 09:11:04 +0000

    Add 27.php

---
+++ src/27.php
@@ -0,0 +1,5 @@
+<?php
+$student = "张三";
+$message = "你好，世界！我是来自项目123456789的$a$b$c$d$e$f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z,A,B,C,D,E,F,G,H,I,J,K,L,M,N,O,P,Q,R,S,T,U,V,W,X,Y,Z,a,b,c,d,e,f";
+echo $message;
+?>
